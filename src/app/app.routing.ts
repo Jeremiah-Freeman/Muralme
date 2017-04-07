79ea443b2d7e21da5007ba4c9e03a69dbc71e52d
jay freeman <jaythinkshappiness@gmail.com>
2017-04-07 08:08:03 -0700

    code up the routing page with needed paths and components

---
+++ src/app/app.routing.ts
@@ -0,0 +1,35 @@
+import { CreateNewComponent } from './create-new/create-new.component';
+import { EditComponent } from './edit/edit.component';
+import { ArtistDetailComponent } from './artist-detail/artist-detail.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
+const appRoutes: Routes = [
+
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'edit',
+    component: EditComponent
+  },
+  {
+    path: 'artistdetail',
+    component: ArtistDetailComponent
+  },
+  {
+    path: 'create',
+    component: CreateNewComponent
+  }
+
+]
