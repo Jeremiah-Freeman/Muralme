52ca03c6edfff8ae06ca27911e8c9ce8b93ae0de
jay freeman <jaythinkshappiness@gmail.com>
2017-04-07 10:23:24 -0700

    add new artist constructor with required entry checker

--- src/app/create-new/create-new.component.ts
+++ src/app/create-new/create-new.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Muralme } from '../muralme.model';
+import { MuralmeService } from '../muralme.service';
 
 
 @Component({
@@ -9,8 +10,19 @@ import { Muralme } from '../muralme.model';
 })
 export class CreateNewComponent implements OnInit {
 
+  addArtist(name: string, bio: string, style: string, paint: string, price: number, imageurl: string) {
+    if (!name || !bio || !style || !paint || !price || !imageurl) {
+      alert("Oops, please fill out all the fields");
+    } else {
+      var newArtist: Artist = new Artist(name, bio, style, paint, price, imageurl);
+      console.log(newArtist);
+      this.muralmeService.saveArtist(newArtist);
+    }
+  }
 
-  constructor() { }
+
+
+  constructor(private muralmeService: MuralmeService) { }
 
   ngOnInit() {
   }
