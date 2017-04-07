3c8e2fbfba6b0dd545902cd2936b54b34b18841f
jay freeman <jaythinkshappiness@gmail.com>
2017-04-07 10:56:28 -0700

    buildout artist-detail backend with all neccessary routing and firebase components with all needed imports

--- src/app/artist-detail/artist-detail.component.ts
+++ src/app/artist-detail/artist-detail.component.ts
@@ -1,16 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { Muralme } from '../muralme.model';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { MuralmeService } from '../muralme.service';
+
 
 @Component({
   selector: 'app-artist-detail',
   templateUrl: './artist-detail.component.html',
-  styleUrls: ['./artist-detail.component.css']
+  styleUrls: ['./artist-detail.component.css'],
+  providers: [MuralmeServices]
 })
 export class ArtistDetailComponent implements OnInit {
+  artistId: string = null;
+  artistToDisplay: Artist;
 
-  constructor() { }
+  constructor(private muralmeService: MuralmeServices, private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.artistId = urlParameters['id'];
+    });
+    this.artistService.getArtistById(this.artistId).subscribe(dataLastEmittedObserver => {
+      this.artistToDisplay = new Artist(
+        dataLastEmittedObserver.name,
+        dataLastEmittedObserver.bio,
+        dataLastEmittedObserver.style,
+        dataLastEmittedObserver.paint,
+        dataLastEmittedObserver.price,
+        dataLastEmittedObserver.imageurl)
+    });
   }
 
 }
