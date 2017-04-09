5b9c002c1dcf375752356b987652c26c0369d4da
Jay Freeman <jaythinkshappiness@gmail.com>
2017-04-08 21:20:51 -0700

    add and fix select menu for pipe

--- src/app/artist.pipe.ts
+++ src/app/artist.pipe.ts
@@ -1,12 +1,53 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Muralme } from './muralme.model';
+
 
 @Pipe({
   name: 'artist'
 })
+
 export class ArtistPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+
+
+  transform(input: Muralme[], desiredArtist) {
+
+    var output: Muralme[] = [];
+
+  if(desiredArtist === "lowCost") {
+
+    for (var i = 0; i < input.length; i++) {
+
+     if (input[i].price < 10000) {
+
+       output.push(input[i]);
+
+     }
+
+   }
+
+   return output;
+
+  } else if (desiredArtist === "highCost") {
+
+    for (var i = 0; i < input.length; i++) {
+
+      if (input[i]. price > 10001) {
+
+       output.push(input[i]);
+
+      }
+
+    }
+
+     return output;
+
+    } else {
+
+     return input;
+
+    }
+
   }
 
 }
